f4c27525e3d8993c0b68cab67c3a6b8ec6d942fd
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-06-25 21:42:39 +0900

    OnSwipe の onTouchUp も試す

--- MotionLayout/onswipe/src/main/java/com/github/mag0716/motionlayoutsample/onswipe/MainActivity.kt
+++ MotionLayout/onswipe/src/main/java/com/github/mag0716/motionlayoutsample/onswipe/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.github.mag0716.motionlayoutsample.onswiperegions
+package com.github.mag0716.motionlayoutsample.onswipe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
