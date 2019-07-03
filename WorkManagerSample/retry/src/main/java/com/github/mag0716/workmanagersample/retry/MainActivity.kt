3c1c9af18a285040ee251ea52b0f56c497a50b7c
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-07-03 22:00:39 +0900

    LoggingWorker をリトライ回数指定で動作

--- WorkManagerSample/retry/src/main/java/com/github/mag0716/workmanagersample/retry/MainActivity.kt
+++ WorkManagerSample/retry/src/main/java/com/github/mag0716/workmanagersample/retry/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.github.mag0716.workmanagersample.retry
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.github.mag0716.common.LoggingWorker
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "Retry"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener { startOneTimeWorkUntilSuccess(3) }
+    }
+
+    private fun startOneTimeWorkUntilSuccess(retryCount: Int) {
+        val work = OneTimeWorkRequestBuilder<LoggingWorker>()
+                .setInputData(LoggingWorker.createInputData(TAG, retryCount))
+                .build()
+        // ENQUEUED -> RUNNING -> SUCCEEDED の順だが、ENQUEUED が出力されない場合もある
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(work.id)
+                .observe(this, Observer { status ->
+                    Log.d(TAG, "observe : status = $status")
+                })
+        WorkManager.getInstance(this).enqueue(work)
     }
 }
