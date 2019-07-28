30bd24c6a6dae9036c85c2fe886934529c043a97
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-07-28 21:59:36 +0900

    ViewModel の値が保持されることを確認

---
+++ NavigationSample/navgraphviewmodels/src/main/java/com/github/mag0716/navigationsample/navgraphviewmodels/SampleViewModel.kt
@@ -0,0 +1,8 @@
+package com.github.mag0716.navigationsample.navgraphviewmodels
+
+import androidx.lifecycle.ViewModel
+
+class SampleViewModel : ViewModel() {
+
+    var count: Int = 0
+}
--- NavigationSample/navgraphviewmodels/src/main/java/com/github/mag0716/navigationsample/navgraphviewmodels/SecondFragment.kt
+++ NavigationSample/navgraphviewmodels/src/main/java/com/github/mag0716/navigationsample/navgraphviewmodels/SecondFragment.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.navGraphViewModels
 import kotlinx.android.synthetic.main.fragment_first.*
 
 class SecondFragment : androidx.fragment.app.Fragment() {
+
+    val activityScopeViewModel: SampleViewModel by activityViewModels()
+    val navGraphScopeViewModel: SampleViewModel by navGraphViewModels(R.id.navigation_graph)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -17,5 +22,10 @@ class SecondFragment : androidx.fragment.app.Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        updateText()
+    }
+
+    private fun updateText() {
+        text.text = "activity=${activityScopeViewModel.count} : navGraph=${navGraphScopeViewModel.count}"
     }
 }
