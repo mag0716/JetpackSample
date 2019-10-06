b3900b70a9ef19ab4e2070feb7ebd7cb46d284d7
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-10-06 20:31:04 +0900

    SavedState with ViewModel の動作確認

--- SavedStateSample/withviewmodel/src/main/java/com/github/mag0716/savedstatesample/withviewmodel/MainActivity.kt
+++ SavedStateSample/withviewmodel/src/main/java/com/github/mag0716/savedstatesample/withviewmodel/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.github.mag0716.savedstatesample.withviewmodel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SavedStateSample"
+    }
+
+    private val mainViewModel: MainViewModel by lazy {
+        ViewModelProviders.of(
+            this,
+            SavedStateViewModelFactory(application, this)
+        ).get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null) {
+            mainViewModel.update()
+        }
+
+        Log.d(TAG, "onCreate : ${mainViewModel.createdTime.value}")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
     }
 }
