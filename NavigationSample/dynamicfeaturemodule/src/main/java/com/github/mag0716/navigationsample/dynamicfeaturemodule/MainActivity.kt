7177a87df77a873c072842947edb9eda6e427187
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-01-27 21:03:38 +0900

    Navigate with dynamic feature modules を試すモジュール追加

---
+++ NavigationSample/dynamicfeaturemodule/src/main/java/com/github/mag0716/navigationsample/dynamicfeaturemodule/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.mag0716.navigationsample.dynamicfeaturemodule
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.container)
+        toolbar.setupWithNavController(navController)
+    }
+}
