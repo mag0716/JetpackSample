7556757bb9e3aaf2100f9490fdf24b7edd2d86bd
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-04-02 21:49:16 +0900

    view モジュールがビルドが通るように修正
    ・アプリ起動でクラッシュする問題あり

---
+++ CameraXSample/view/src/main/java/com/github/mag0716/camerax/view/App.kt
@@ -0,0 +1,11 @@
+package com.github.mag0716.camerax.view
+
+import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+
+class App : Application(), CameraXConfig.Provider {
+    override fun getCameraXConfig(): CameraXConfig {
+        return Camera2Config.defaultConfig()
+    }
+}
