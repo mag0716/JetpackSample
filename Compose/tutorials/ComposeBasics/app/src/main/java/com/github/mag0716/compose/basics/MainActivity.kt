1513f4fb6e5bde45de0bc08e64c6b3795ea516d7
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-08-31 21:25:17 +0900

    Lesson2：Add a picture

--- Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
+++ Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
@@ -2,12 +2,14 @@ package com.github.mag0716.compose.basics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
@@ -22,9 +24,20 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
+    val image = imageResource(id = R.drawable.header)
     Column(
             modifier = Modifier.padding(16.dp)
     ) {
+        val imageModifier = Modifier
+                .preferredHeight(180.dp)
+                .fillMaxWidth()
+        Image(
+                image,
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
+        )
+        Spacer(Modifier.preferredHeight(16.dp))
+
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
