66f36c463174add79a16dc2c6de9db2067d567be
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-08-31 21:21:27 +0900

    Lesson2：Add style settings to the column

--- Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
+++ Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
-    Column {
+    Column(
+            modifier = Modifier.padding(16.dp)
+    ) {
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
