07dd88010be305e7045a5e58d1f505643e3f811b
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-08-31 21:13:05 +0900

    Lesson1：Add a text element

--- Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
+++ Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
@@ -3,36 +3,13 @@ package com.github.mag0716.compose.basics
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
-import com.github.mag0716.compose.basics.ui.ComposeBasicsTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello world!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeBasicsTheme {
-        Greeting("Android")
     }
 }
