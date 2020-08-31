11b6e5e0469986417aa87207a14c8a9e20643d7f
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-08-31 21:14:42 +0900

    Lesson1：Define a composable function

--- Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
+++ Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
@@ -3,13 +3,19 @@ package com.github.mag0716.compose.basics
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            Greeting("Android")
         }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
