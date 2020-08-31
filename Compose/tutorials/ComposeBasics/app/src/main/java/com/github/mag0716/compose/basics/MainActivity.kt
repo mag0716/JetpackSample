eafe579634b42dd61d9f493608bf838224377790
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-08-31 21:36:45 +0900

    Lesson3：Style the text

--- Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
+++ Compose/tutorials/ComposeBasics/app/src/main/java/com/github/mag0716/compose/basics/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
@@ -27,23 +29,33 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun NewsStory() {
     val image = imageResource(id = R.drawable.header)
-    Column(
-            modifier = Modifier.padding(16.dp)
-    ) {
-        val imageModifier = Modifier
-                .preferredHeight(180.dp)
-                .fillMaxWidth()
-                .clip(shape = RoundedCornerShape(4.dp))
-        Image(
-                image,
-                modifier = imageModifier,
-                contentScale = ContentScale.Crop
-        )
-        Spacer(Modifier.preferredHeight(16.dp))
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+                modifier = Modifier.padding(16.dp)
+        ) {
+            val imageModifier = Modifier
+                    .preferredHeight(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp))
+            Image(
+                    image,
+                    modifier = imageModifier,
+                    contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.preferredHeight(16.dp))
 
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+            Text("A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis)
+            Text("Davenport, California",
+                    style = typography.body2)
+            Text("December 2018",
+                    style = typography.body2)
+        }
     }
 }
 
