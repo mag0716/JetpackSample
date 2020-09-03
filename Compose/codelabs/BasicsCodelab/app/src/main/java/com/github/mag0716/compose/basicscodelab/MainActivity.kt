f86133aefe24adfd7709e07bf1fd0301406e69ad
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-09-03 20:30:06 +0900

    2. Starting a new Compose project

---
+++ Compose/codelabs/BasicsCodelab/app/src/main/java/com/github/mag0716/compose/basicscodelab/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.github.mag0716.compose.basicscodelab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.github.mag0716.compose.basicscodelab.ui.BasicsCodelabTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greeting("Android")
+    }
+}
