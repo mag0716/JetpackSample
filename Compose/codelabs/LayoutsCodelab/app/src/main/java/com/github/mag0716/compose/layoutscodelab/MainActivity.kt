67bca7295d9e7bfd5d540cc2bea83750f0735543
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-09-04 21:49:25 +0900

    3. Modifiers

--- Compose/codelabs/LayoutsCodelab/app/src/main/java/com/github/mag0716/compose/layoutscodelab/MainActivity.kt
+++ Compose/codelabs/LayoutsCodelab/app/src/main/java/com/github/mag0716/compose/layoutscodelab/MainActivity.kt
@@ -3,10 +3,25 @@ package com.github.mag0716.compose.layoutscodelab
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.EmphasisAmbient
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideEmphasis
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.github.mag0716.compose.layoutscodelab.ui.LayoutsCodelabTheme
 
@@ -34,5 +49,43 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     LayoutsCodelabTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { /* Ignoring onClick */ })
+            .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.preferredSize(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .gravity(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            ProvideEmphasis(EmphasisAmbient.current.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutsCodelabTheme {
+        PhotographerCard()
     }
 }
