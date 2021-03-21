3a242e85cad7b970f99d5506f21ad414fb9ade3c
Masashi Kishimoto <mkishimoto0716@gmail.com>
2021-03-21 10:44:24 +0900

    Android Dev Challenge #3用のテーマをコード上に定義

--- Compose/samples/ComposeSamples/app/src/main/java/com/github/mag0716/composesamples/ui/androiddevchallenge3/AndroidDevChallenge3Activity.kt
+++ Compose/samples/ComposeSamples/app/src/main/java/com/github/mag0716/composesamples/ui/androiddevchallenge3/AndroidDevChallenge3Activity.kt
@@ -1,6 +1,19 @@
 package com.github.mag0716.composesamples.ui.androiddevchallenge3
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import com.github.mag0716.composesamples.ui.theme.AndroidDevChallenge3Theme
 
 class AndroidDevChallenge3Activity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidDevChallenge3Theme {
+                Text("Android Dev Challenge #3")
+            }
+        }
+    }
 }
--- Compose/samples/ComposeSamples/app/src/main/java/com/github/mag0716/composesamples/ui/theme/Color.kt
+++ Compose/samples/ComposeSamples/app/src/main/java/com/github/mag0716/composesamples/ui/theme/Color.kt
@@ -6,3 +6,12 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// Android Dev Challeng #3
+val Pink100 = Color(0xFFFFF1F1)
+val Pink900 = Color(0xFF3F2C2C)
+val Green300 = Color(0xFF2D3B2D)
+val Green900 = Color(0xFFB8C9B8)
+val Gray = Color(0xFF232323)
+val White150 = Color(0x26FFFFFF)
+val White850 = Color(0xD8FFFFFF)
