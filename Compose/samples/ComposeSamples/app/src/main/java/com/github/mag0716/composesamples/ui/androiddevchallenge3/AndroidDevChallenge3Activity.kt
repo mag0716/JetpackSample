b45523e0f1daf6aabb69c2157d492be481a85eac
Masashi Kishimoto <mkishimoto0716@gmail.com>
2021-03-21 16:13:20 +0900

    ステータスバーを透明化

--- Compose/samples/ComposeSamples/app/src/main/java/com/github/mag0716/composesamples/ui/androiddevchallenge3/AndroidDevChallenge3Activity.kt
+++ Compose/samples/ComposeSamples/app/src/main/java/com/github/mag0716/composesamples/ui/androiddevchallenge3/AndroidDevChallenge3Activity.kt
@@ -1,6 +1,7 @@
 package com.github.mag0716.composesamples.ui.androiddevchallenge3
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
@@ -10,6 +11,9 @@ class AndroidDevChallenge3Activity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // FIXME: use WindowInsets
+        findViewById<View>(android.R.id.content).systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         setContent {
             AndroidDevChallenge3Theme {
                 Text("Android Dev Challenge #3")
