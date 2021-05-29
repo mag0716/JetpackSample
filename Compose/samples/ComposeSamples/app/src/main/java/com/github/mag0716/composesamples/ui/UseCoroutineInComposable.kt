d70d51da67a5b251a1c2dc1ea575bc0f224af389
Masashi Kishimoto <mkishimoto0716@gmail.com>
2021-05-30 08:01:42 +0900

    rememberCoroutineScope()を試す

--- Compose/samples/ComposeSamples/app/src/main/java/com/github/mag0716/composesamples/ui/UseCoroutineInComposable.kt
+++ Compose/samples/ComposeSamples/app/src/main/java/com/github/mag0716/composesamples/ui/UseCoroutineInComposable.kt
@@ -2,10 +2,12 @@ package com.github.mag0716.composesamples.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import kotlinx.coroutines.launch
 
 @Composable
 fun LaunchedEffectSampleScreen(
@@ -34,11 +36,27 @@ private fun LaunchedEffectSample(
     isError: Boolean,
     scaffoldState: ScaffoldState
 ) {
+    // LaunchedEffectもrememberCoroutineScopeも@Composableでsuspend funを実行するためのもの
+    // @Composableの直下はLaunchedEffect, @Composable直下でない場合はrememberCoroutineScope()
+    // @ComposableがCompositionから削除された場合はCoroutineはキャンセルされるのは同じ動作
+    val coroutineScope = rememberCoroutineScope()
     if (isError) {
         LaunchedEffect(scaffoldState.snackbarHostState) {
             scaffoldState.snackbarHostState.showSnackbar(
                 message = "Error message"
             )
         }
+    }
+
+    Button(
+        onClick = {
+            coroutineScope.launch {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = "Message"
+                )
+            }
+        }
+    ) {
+        Text("Show SnackBar")
     }
 }
